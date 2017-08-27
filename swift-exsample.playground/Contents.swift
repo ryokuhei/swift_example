18966a7bad93f9dc643b5680dfc2c0d7189c899d
ryokuhei <r.sato@solt-inc.com>
2017-08-27 12:38:28 +0900

    table of contents

---
+++ swift-exsample.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+// Swift 3
+
+import UIKit
+/* Basic Syntax */
+// Array p.47
+// Dictionary p.51
+// Range p.54
+// Optional p.58
+// Tuple p.69
+// Cast p.71
+// If p.77
+// For p.90
+// Fallthrough p.94
+// Defer p.99
+// Inout p.110
+// Closure p.113
+// Stored Property p.136
+// Computed Property p.138
+// Initializer p.141
+// Subscript p.150
+// Extension p.153
+// Mutating p.162
+
+/*　Type */
+// Struct p.165
+// Class p.168
+// Enum p.182
+// Protocol p.191
+// Generics p.221
+// AccessLevel p.240
+
+/* Notification */
+// DelegatePattern p.271
+// Closer(CallBack) p.276
+// ObserverPattern p.285
+
+/* Asynchronous */
+// GCD(Grand Central Dispatch) p.293
+// Operation,OperationQueue p.298
+// Thread p.305
+
+/* Error handling */
+// Optional<Wrapped> p.311
+// Result<T,Error> p.313
+// do-catch p.318
+// fatalError(_:) p.336
