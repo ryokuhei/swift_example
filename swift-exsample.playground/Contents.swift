47bf8995871c14873821e488784f2ca19c0e05a7
ryokuhei <r.sato@solt-inc.com>
2017-09-01 22:06:25 +0900

    range

--- swift-exsample.playground/Contents.swift
+++ swift-exsample.playground/Contents.swift
@@ -5,51 +5,52 @@ import UIKit
 
 //import BasicSyntax
 /* Basic Syntax */
-// Array p.47
-//// <#Description#>let array = BasicSyntax.Array()
+// -Array p.47
+// BasicSyntax._Array()
+// -Dictionary p.51
+// BasicSyntax._Dictionary()
 
-// Dictionary p.51
-var dictionary = BasicSyntax.Dictionary()
-// Range p.54
-// Optional p.58
-// Tuple p.69
-// Cast p.71
-// If p.77
-// For p.90
-// Fallthrough p.94
-// Defer p.99
-// Inout p.110
-// Closure p.113
-// Stored Property p.136
-// Computed Property p.138
-// Initializer p.141
-// Subscript p.150
-// Extension p.153
-// Mutating p.162
+// -Range p.54
+BasicSyntax._Range()
+// -Optional p.58
+// -Tuple p.69
+// -Cast p.71
+// -If p.77
+// -For p.90
+// -Fallthrough p.94
+// -Defer p.99
+// -Inout p.110
+// -Closure p.113
+// -Stored Property p.136
+// -Computed Property p.138
+// -Initializer p.141
+// -Subscript p.150
+// -Extension p.153
+// -Mutating p.162
 
 /*　Type */
-// Struct p.165
-// Class p.168
-// Enum p.182
-// Protocol p.191
-// Generics p.221
-// AccessLevel p.240
+// -Struct p.165
+// -Class p.168
+// -Enum p.182
+// -Protocol p.191
+// -Generics p.221
+// -AccessLevel p.240
 
 /* Notification */
-// DelegatePattern p.271
-// Closer(CallBack) p.276
-// ObserverPattern p.285
+// -DelegatePattern p.271
+// -Closer(CallBack) p.276
+// -ObserverPattern p.285
 
 /* Asynchronous */
-// GCD(Grand Central Dispatch) p.293
-// Operation,OperationQueue p.298
-// Thread p.305
+// -GCD(Grand Central Dispatch) p.293
+// -Operation,OperationQueue p.298
+// -Thread p.305
 
 /* Error handling */
-// Optional<Wrapped> p.311
-// Result<T,Error> p.313
-// do-catch p.318
-// fatalError(_:) p.336
+// -Optional<Wrapped> p.311
+// -Result<T,Error> p.313
+// -do-catch p.318
+// -fatalError(_:) p.336
 
 
 Test.main()
--- swift-exsample.playground/Sources/BasicSyntax.swift
+++ swift-exsample.playground/Sources/BasicSyntax.swift
@@ -3,41 +3,41 @@ import Foundation
 public struct BasicSyntax {
 
     // p.47
-    public class Array {
+    public class _Array {
 
-       public var array = ["a1", "a2", "a3"]
-       public var array2: [Int] = [1, 2, 3]
-       public var array3: [String]? = []
-       public var array4 = [Int]()
-       public var dimensionalArray = [[1, 2, 3], [4, 5, 6]]
-       public var dimensionalArray2: [[String]]? = []
+        public var array = ["a1", "a2", "a3"]
+        public var array2: [Int] = [1, 2, 3]
+        public var array3: [String]? = []
+        public var array4 = [Int]()
+        public var dimensionalArray = [[1, 2, 3], [4, 5, 6]]
+        public var dimensionalArray2: [[String]]? = []
 
-       public init() {
-                    print("show array")
-self.show()
-print()
+        public init() {
+            print("show array")
+            self.show()
+            print()
 
-print("add array")
-self.add("add")
-self.add(5)
-self.show()
-print()
+            print("add array")
+            self.add("add")
+            self.add(5)
+            self.show()
+            print()
 
-print("update array")
-self.update("update")
-self.update(10)
-self.show()
-print()
+            print("update array")
+            self.update("update")
+            self.update(10)
+            self.show()
+            print()
 
-print("join array")
-self.join(array, array3!)
-self.join(array2, array4)
-self.show()
-print()
+            print("join array")
+            self.join(array, array3!)
+            self.join(array2, array4)
+            self.show()
+            print()
 
         }
 
-       public func show() {
+        public func show() {
             print("array \t\t\t\t: ", terminator: "")
             print(array)
 
@@ -61,9 +61,9 @@ print()
 
             print("dimensionalArray2 \t: ", terminator: "")
             print(dimensionalArray2!)
-       }
+        }
 
-         public func add(_ value: String) {
+        public func add(_ value: String) {
             array.append(value)
             array3?.append(value)
             dimensionalArray2?.append(array3!)
@@ -71,14 +71,14 @@ print()
             print("-value: \(value) added....")
         }
 
-       public func add(_ value: Int) {
+        public func add(_ value: Int) {
             array2.append(value)
             array4.append(value)
             dimensionalArray.append(array2)
 
             print("-value: \(value) added....")
         }
-         public func update(_ value: String) {
+        public func update(_ value: String) {
             array[0] = value
             array3?[0] = value
             dimensionalArray2?[0] = array3!
@@ -86,7 +86,7 @@ print()
             print("-value: \(value) updated....")
         }
 
-         public func update(_ value: Int) {
+        public func update(_ value: Int) {
             array2[0] = value
             array4[0] = value
             dimensionalArray[0] = array2
@@ -106,32 +106,32 @@ print()
     }
 
     // p.51
-    public class Dictionary {
+    public class _Dictionary {
 
         public var dictionary = ["a": 1, "b": 2]
         public var dictionary2: [String: String] = [:]
 
         public init() {
-print("show dictionary")
-self.show()
-print()
+            print("show dictionary")
+            self.show()
+            print()
 
-print("add dictionary")
-self.add(key: "key", value: 0)
-self.add(key: "key", value: "add")
-self.show()
-print()
+            print("add dictionary")
+            self.add(key: "key", value: 0)
+            self.add(key: "key", value: "add")
+            self.show()
+            print()
 
-print("update dictionary")
-self.update(key: "key", value: 5)
-self.update(key: "key", value: "update")
-self.show()
-print()
+            print("update dictionary")
+            self.update(key: "key", value: 5)
+            self.update(key: "key", value: "update")
+            self.show()
+            print()
 
-print("delete dictionary")
-self.delete(key: "key")
-self.show()
-print()
+            print("delete dictionary")
+            self.delete(key: "key")
+            self.show()
+            print()
 
         }
 
@@ -179,5 +179,97 @@ print()
 
             print("-key :\(key) deleted....")
         }
+    }
+
+    // p.54
+    public class _Range {
+
+        public init() {
+            print("[Range]")
+            onRange(start: 1, end: 10, containtsNo: 2)
+            print()
+
+            print("[CountableRange]")
+            onCountableRange(start: 1, end: 10, containtsNo: 2)
+            print()
+
+            print("[ClosedRange]")
+            onClosedRange(start: 1, end: 10, containtsNo: 2)
+            print()
+
+            print("[CountableClosedRange]")
+            onCountableClosedRange(start: 1, end: 10, containtsNo: 2)
+            print()
+        }
+
+        public func onRange(start: Int, end: Int, containtsNo: Int? = nil) {
+            let range: Range<Int>  = start ..< end
+
+            print("lower: \(range.lowerBound)")
+            print("upper: \(range.upperBound)")
+            print("count: \(range.count)")
+            // Error : Range in not countable
+/*
+            for vaue in range {
+                print(value)
+            }
+*/
+            if  let containtsNo = containtsNo {
+                print("contains \(containtsNo): \(range.contains(containtsNo))")
+            }
+
+        }
+
+        public func onCountableRange(start: Int, end: Int, containtsNo: Int? = nil) {
+            let countableRange: CountableRange<Int>  = start ..< end
+
+            print("lower: \(countableRange.lowerBound)")
+            print("upper: \(countableRange.upperBound)")
+            print("count: \(countableRange.count)")
+
+            print("value:")
+            for value in countableRange {
+                print(value)
+            }
+
+            if  let containtsNo = containtsNo {
+                print("contains \(containtsNo): \(countableRange.contains(containtsNo))")
+            }
+        }
+
+        public func onClosedRange(start: Int, end: Int, containtsNo: Int? = nil) {
+            let closedRange: ClosedRange<Int>  = start ... end
+
+            print("lower: \(closedRange.lowerBound)")
+            print("upper: \(closedRange.upperBound)")
+            print("count: \(closedRange.count)")
+            // Error : ClosedRange in not countable
+/*
+            for vaue in range {
+                print(value)
+            }
+*/
+            if  let containtsNo = containtsNo {
+                print("contains \(containtsNo): \(closedRange.contains(containtsNo))")
+            }
+        }
+
+        public func onCountableClosedRange(start: Int, end: Int, containtsNo: Int? = nil) {
+            let countableClosedRange: CountableClosedRange<Int>  = start ... end
+
+            print("lower: \(countableClosedRange.lowerBound)")
+            print("upper: \(countableClosedRange.upperBound)")
+            print("count: \(countableClosedRange.count)")
+
+            print("value:")
+            for value in countableClosedRange {
+                print(value)
+            }
+            if  let containtsNo = containtsNo {
+                print("contains \(containtsNo): \(countableClosedRange.contains(containtsNo))")
+            }
+
+        }
+
     }
 }
